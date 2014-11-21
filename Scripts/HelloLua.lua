7925d3f390879a6075a6ababc7416a0b1f23e005
RadiantGrandFowl <RadiantGrandFowl@outlook.com>
2014-11-21 21:54:08 +0100

    Update HelloLua.lua

--- Scripts/HelloLua.lua
+++ Scripts/HelloLua.lua
@@ -1,11 +1,10 @@
-
+local F12 = drawMgr:CreateFont("F11","Arial",12,500)
 
 function Tick(tick)
-    if not client.connected or client.loading or client.chat then 
-        return 
-    end
-    client.ExecuteCmd("Say hello_world")
-    Sleep(5000)
-end
 
-script:RegisterEvent(EVENT_TICK,Tick)
+	if not client.connected or client.loading or client.chat then 
+        return 
+	end
+		CreateText([100, 100, 0xFFFFFFFF , "Hello World", F12)
+		
+end
